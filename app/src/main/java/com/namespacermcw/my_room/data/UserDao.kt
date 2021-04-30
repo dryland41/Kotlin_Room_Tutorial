636b1d7cbb7ec8c33c76ec7c8b69138115ec39cf
Rylan Wassem <rylan@ravebizz.com>
2021-04-30 19:54:59 -0400

    Created database scheme

---
+++ app/src/main/java/com/namespacermcw/my_room/data/UserDao.kt
@@ -0,0 +1,18 @@
+package com.namespacermcw.my_room.data;
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+public interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addUser(user: User)
+
+    @Query("SELECT * FROM user_table ORDER BY id ASC")
+    fun readAllData(): LiveData<List<User>>
+
+}
